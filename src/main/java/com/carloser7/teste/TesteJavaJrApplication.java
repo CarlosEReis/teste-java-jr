68fbbf9c450483025bb6984fa5eefd017c6f8574
CarlosEReis <carlos.er7@gmail.com>
2024-01-27 12:31:07 -0300

    build: commit inicial

---
+++ src/main/java/com/carloser7/teste/TesteJavaJrApplication.java
@@ -0,0 +1,13 @@
+package com.carloser7.teste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteJavaJrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteJavaJrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carloser7/teste/TesteJavaJrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carloser7.teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteJavaJrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
